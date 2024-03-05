2fda0f023a0e89fcdb7dae13246ef273888f45ac
brunodiazm <brunodiazll205@gmail.com>
2024-03-05 20:17:50 -0300

    Mi segundo commit

---
+++ front/scripts/index.js
@@ -0,0 +1,25 @@
+const allMoviesSection = document.getElementById("allMovies");
+
+tempData.forEach((movie) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const image = document.createElement("img");
+  image.classList.add("card-image");
+  image.src = movie.poster;
+  image.alt = movie.title;
+
+  const info = document.createElement("div");
+  info.classList.add("card-info");
+  info.innerHTML = `
+    <h4 class="titulo">${movie.title} (${movie.year})</h4>
+    <p class="director">${movie.director}</p>
+    <p class="duracion">${movie.duration}</p>
+    <p class="genero">${movie.genre.join(", ")}</p>
+    <p class="calificacion">Calificacion: ${movie.rate}</p>
+  `;
+
+  card.appendChild(image);
+  card.appendChild(info);
+  allMoviesSection.appendChild(card);
+});
