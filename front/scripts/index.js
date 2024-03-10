9816aa90ba0610970a1ab7e9015a62f4de9b0194
brunodiazm <brunodiazll205@gmail.com>
2024-03-10 04:09:39 -0300

    Mi segundo commit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,31 +1,5 @@
-const allMoviesSection = document.getElementById("allMovies");
+const renderCards = require("./renderCards");
 
 $.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
   renderCards(data);
 });
-
-const renderCards = (data) => {
-  data.forEach((movie) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const image = document.createElement("img");
-    image.classList.add("card-image");
-    image.src = movie.poster;
-    image.alt = movie.title;
-
-    const info = document.createElement("div");
-    info.classList.add("card-info");
-    info.innerHTML = `
-    <h4 class="titulo">${movie.title} (${movie.year})</h4>
-    <p class="director">${movie.director}</p>
-    <p class="duracion">${movie.duration}</p>
-    <p class="genero">${movie.genre.join(", ")}</p>
-    <p class="calificacion">Calificacion: ${movie.rate}</p>
-  `;
-
-    card.appendChild(image);
-    card.appendChild(info);
-    allMoviesSection.appendChild(card);
-  });
-};
