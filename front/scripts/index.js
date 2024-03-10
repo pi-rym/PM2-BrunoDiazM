65fa9b4aa90ef228a7c104f7f5d7de14545e4902
brunodiazm <brunodiazll205@gmail.comgit config --global user.email brunodiazll205@gmail.com>
2024-03-10 01:47:35 -0300

    Mi segundo commit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,17 +1,20 @@
 const allMoviesSection = document.getElementById("allMovies");
 
-tempData.forEach((movie) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
+$.get("https://students-api.2.us-1.fl0.io/movies", (data) => renderCards(card));
 
-  const image = document.createElement("img");
-  image.classList.add("card-image");
-  image.src = movie.poster;
-  image.alt = movie.title;
+const renderCards = (data) => {
+  data.forEach((movie) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-  const info = document.createElement("div");
-  info.classList.add("card-info");
-  info.innerHTML = `
+    const image = document.createElement("img");
+    image.classList.add("card-image");
+    image.src = movie.poster;
+    image.alt = movie.title;
+
+    const info = document.createElement("div");
+    info.classList.add("card-info");
+    info.innerHTML = `
     <h4 class="titulo">${movie.title} (${movie.year})</h4>
     <p class="director">${movie.director}</p>
     <p class="duracion">${movie.duration}</p>
@@ -19,11 +22,8 @@ tempData.forEach((movie) => {
     <p class="calificacion">Calificacion: ${movie.rate}</p>
   `;
 
-  card.appendChild(image);
-  card.appendChild(info);
-  allMoviesSection.appendChild(card);
-});
-
-$.get("https://students-api.2.us-1.fl0.io/movie", (data) =>
-  data.forEach(movie)
-);
+    card.appendChild(image);
+    card.appendChild(info);
+    allMoviesSection.appendChild(card);
+  });
+};
