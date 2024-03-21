f424ea6121b28e1894a8cd9e3196239bc83fb4e2
brunodiazm <brunodiazll205@gmail.com>
2024-03-21 07:07:11 -0300

    Mi segundo commit

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -13,26 +13,3 @@ async function getFilms() {
 
 module.exports = getFilms;
 
-// handler.js
-// const axios = require("axios");
-// const renderCards = require("./renderCards");
-
-// async function getFilms() {
-//   try {
-//     const response = await axios.get(
-//       "https://students-api.up.railway.app/movies"
-//     );
-//     const films = response.data;
-//     const allMoviesContainer = document.getElementById('allMovies');
-//     if (allMoviesContainer) {
-//       renderCards(films);
-//     } else {
-//       console.error("El contenedor 'allMovies' no se encontró en el DOM.");
-//     }
-//   } catch (error) {
-//     console.error("Error al obtener las películas:", error);
-//   }
-// }
-
-// module.exports = getFilms;
-
