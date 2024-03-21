9fca3035c7429e5a6bfb288a718061d18f6dcace
brunodiazm <brunodiazll205@gmail.com>
2024-03-21 06:49:13 -0300

    Mi segundo commit

--- challenge-testing/index.js
+++ challenge-testing/index.js
@@ -1,3 +1,25 @@
-class CarritoCompra {}
+class CarritoCompra {
+  constructor() {
+    this.carrito = [];
+  }
+
+  agregarProducto(producto) {
+    this.carrito.push(producto);
+  }
+
+  calcularTotal() {
+    return this.carrito.reduce((total, producto) => total + producto.precio, 0);
+  }
+
+  aplicarDescuento(porcentaje) {
+    if (porcentaje < 0 || porcentaje > 100) {
+      throw new Error("El porcentaje de descuento debe estar entre 0 y 100");
+    }
+    const total = this.calcularTotal();
+    const descuento = total * (porcentaje / 100);
+    return total - descuento;
+  }
+}
 
 module.exports = CarritoCompra;
+
